0854f2f2533366ee2baae201123211397159306c
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-02 19:09:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if !position_taken?(board,index)&& board[index].between?(0,8)
+ !position_taken?(board,index)&& board[index].between?(0,8)
     true
-  else 
-    false
     end
 end
 
